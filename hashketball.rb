917d200b9208cc0c23795f9a569d5514ce354781
Zoncharne Lee <zoncharne@gmail.com>
2019-03-07 23:49:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,13 @@
 def game_hash
   game_hash = {
     :home => {
-      
+      :team_name 
+      :colors
+      :players
     }
     :away => {
-      
+      :team_name
+      :colors
+      :players 
     }
   }
