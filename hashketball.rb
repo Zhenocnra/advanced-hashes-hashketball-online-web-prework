2ef8366912ea83252be62ec53f7bbc6b4a98fe77
Zoncharne Lee <zoncharne@gmail.com>
2019-03-07 23:51:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,13 +5,22 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         name => {
-          
+          :number
+          :shoe
+          :points
+          :rebounds
+          :assists
+          :steals
+          :blocks
+          :slam_dunks}
         }
       }
     }
     :away => {
       :team_name
       :colors
-      :players 
+      :players => {
+        
+      }
     }
   }
